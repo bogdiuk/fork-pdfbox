f6771fec3cd2baa305b61dda98942fbe6ae9918e
Mykola Bohdiuk <mykola.bogdiuk@gmail.com>
2024-07-24 16:37:57 +0300

    [FIX] Sometimes RadioGroup doesn't have FLAG_RADIO set, but its child has

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDFieldFactory.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/form/PDFieldFactory.java
@@ -23,6 +23,7 @@ import org.apache.pdfbox.cos.COSArray;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSDictionary;
 import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.COSObject;
 
 /**
  * A PDField factory.
@@ -115,7 +116,8 @@ public final class PDFieldFactory
         // BJL: I have found that the radio flag bit is not always set
         // and that sometimes there is just a kids dictionary.
         // so, if there is a kids dictionary then it must be a radio button group.
-        if ((flags & PDButton.FLAG_RADIO) != 0)
+        if ((flags & PDButton.FLAG_RADIO) != 0
+                || containsRadios(field.getCOSArray(COSName.KIDS))) // "|| containsRadios(...)" is a CrickSoft addition
         {
             return new PDRadioButton(form, field, parent);
         }
@@ -127,6 +129,28 @@ public final class PDFieldFactory
         {
             return new PDCheckBox(form, field, parent);
         }
+    }
+
+    private static boolean containsRadios(COSArray kids) // CrickSoft addition
+    {
+        if (kids != null)
+        {
+            for (COSBase kidId : kids)
+            {
+                if (kidId instanceof COSObject)
+                {
+                    final COSBase kid = ((COSObject) kidId).getObject();
+                    if (kid instanceof COSDictionary)
+                    {
+                        if ((((COSDictionary) kid).getInt(COSName.FF, 0) & PDButton.FLAG_RADIO) != 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     private static String findFieldType(COSDictionary dic, Set<COSDictionary> seen)
